e6ab516c022fa5f007d9962a9896290691ee76dd
sosohungry <799238605@qq.com>
2022-01-09 10:18:13 +0800

    web design

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask import request, render_template
 app = Flask(__name__)
 
 
@@ -8,5 +8,25 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+
+
+
+@app.route("/login",methods=['GET'])   #请求方式为get
+def login():
+    return render_template('login.html')
+
+
+# @app.route("/login",methods=['POST']) #请求方式为post
+# def loginin():
+#     if request.form['username']=='zkx' and request.form['password']=='zkx':
+#             username =request.form['username']
+#             return render_template('l.html',username='zkx',moban='moban')
+#     return render_template('one.html',username='username',moban='shurucuowu')
+
+@app.route("/register", methods=["GET"])
+def register():
+    return render_template('register.html')
+
+    
 if __name__ == '__main__':
     app.run()
